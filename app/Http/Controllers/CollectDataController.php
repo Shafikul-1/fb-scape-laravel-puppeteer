632c6915078a99cfff8d00ea4422827db815b339
shafik <shaifk108@gmail.com>
2024-08-29 13:52:39 +0600

    work

--- app/Http/Controllers/CollectDataController.php
+++ app/Http/Controllers/CollectDataController.php
@@ -11,18 +11,20 @@ use Illuminate\Support\Facades\File;
 
 class CollectDataController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $allData = CollectData::all();
         // return $allData;
         return view('fbData.allData', compact('allData'));
     }
 
-    public function collectData(){
+    public function collectData()
+    {
         $jsonFile = base_path('resources/js/fbData.json');
-        if(File::exists($jsonFile)){
+        if (File::exists($jsonFile)) {
             $fbData = json_decode(File::get($jsonFile), true);
             // return $fbData;
-            $collectData = array_map(function ($data){
+            $collectData = array_map(function ($data) {
                 return [
                     'url' => $data['postDetails']['url'] ?? 'connecton Error',
                     'allInfo' => json_encode($data),
@@ -32,21 +34,28 @@ class CollectDataController extends Controller
             }, $fbData);
             $batchSize = 200;
             $chunks = array_chunk($collectData, $batchSize);
-            foreach($chunks as $chunk){
+            foreach ($chunks as $chunk) {
                 CollectData::insert($chunk);
             }
             File::delete($jsonFile);
         }
 
-        $getData =  AllLink::where('check', '=', 'valid')->limit(10)->pluck('link')->toArray();
+        //$getData =  AllLink::where('check', '=', 'valid', '&&', 'status', '=', 'noaction')->limit(10)->pluck('link')->toArray();
+        $getData = AllLink::where('check', 'valid')
+            ->where('status', 'noaction')
+            ->limit(10)
+            ->pluck('link')
+            ->toArray();
 
         try {
-           DatasCollectJob::dispatch($getData);
+            DatasCollectJob::dispatch($getData);
+            foreach ($getData as $key => $value) {
+                AllLink::where('link', '=', $value)->delete();
+            }
             return 'ok';
         } catch (\Throwable $th) {
             throw $th;
         }
-
     }
 
     public function store(Request $request)
