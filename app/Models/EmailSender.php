71fd32e513f76b3ad28a0b8993a42c5bc4be1df6
shafikul <shafikul.18288@gmail.com>
2024-09-01 22:04:19 +0600

    correction

--- app/Models/EmailSender.php
+++ app/Models/EmailSender.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class EmailSender extends Model
 {
     use HasFactory;
+    protected $table = 'email_sender';
 
     // public function emailData(){
     //     return $this->belongsToMany( User::class, 'email_data', 'user_id', 'email_sender_id' );
