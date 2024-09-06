fb6ccac58558a1686a6aa97af13d16a97cf9a080
shafikul <shafikul.18288@gmail.com>
2024-09-06 23:17:58 +0600

    css add

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="stylesheet" href="{{ asset('build/assets/app-D2O3vmEp.css') }}">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
